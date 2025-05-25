35a36a089b177a5d008c15fb949a915e42115b58
migu3lone <mi.gu3lo.ne@gmail.com>
2025-05-24 23:23:07 -0500

    SikuliX

---
+++ SikuliX/prueba.sikuli/prueba.py
@@ -0,0 +1,18 @@
+if exists("pagina_principal.png", 30):
+
+
+    click("usuario.png")
+
+
+    click("ingresar_usuario.png")
+    type ('alonso')
+
+
+    click("ingresar_contraseña.png")
+    
+    type ('hola')
+
+    click("aceptar.png")
+
+
+    click("ingresar.png")
